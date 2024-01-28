df3ac3204dff23df5baa890ab4acd2bd5f6b79a1
dat586 <32485424+dat586@users.noreply.github.com>
2024-01-28 17:42:16 -0500

    Update high_cpu_mem.sh

--- high_cpu_mem.sh
+++ high_cpu_mem.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-topHeader=`top -b -n 1 | sed -n '7p'`
-topOutput=`top -b -n 1 | sed '1,7d'`
+topHeader=$(top -b -n 1 | sed -n '7p')
+topOutput=$(top -b -n 1 | sed '1,7d')  # Using top to avoid permission issues
 IFS='
-'
+'  # Change field separator to new line
 
-declare -a report=()
-report+=("$topHeader")
-for process in $topOutput
+declare -a report=()  # Declare empty report array
+report+=("$topHeader")  # Write top header to report
+for process in $topOutput  # Loop over processes
 do
-    cpu=$(echo "$process" | awk '{print $9}')
-    mem=$(echo "$process" | awk '{print $10}')
-    cpuHigh=$(echo "$cpu > 50" | bc -l)
-    memHigh=$(echo "$mem > 50" | bc -l)
+    cpu=$(echo "$process" | awk '{print $9}')  # Find CPU metric and assign
+    mem=$(echo "$process" | awk '{print $10}')  # Find memory metric and assign
+    cpuHigh=$(echo "$cpu > 50" | bc -l)  # Evaluate if CPU is over 50%, returns 1 if true
+    memHigh=$(echo "$mem > 50" | bc -l)  # Evaluate if CPU is over 50%, returns 1 if true
     if [[ $cpuHigh == 1 || $memHigh == 1 ]]; then
-        report+=("$process")
+        report+=("$process")  # If CPU or memory is over 50%, write the process to the report
     fi
 done
 
-echo "${report[*]}"
+echo "${report[*]}"  # Display report
